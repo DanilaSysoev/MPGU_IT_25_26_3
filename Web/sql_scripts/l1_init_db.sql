44eb3c969df332266ee36e63c9829d900f5da878
Данила Сысоев <crocodil-msu@ya.ru>
2025-10-06 10:05:52 +0300

    Add information about literals

--- Web/sql_scripts/l1_init_db.sql
+++ Web/sql_scripts/l1_init_db.sql
@@ -9,29 +9,29 @@ CREATE TABLE IF NOT EXISTS demo_1.fruits (
 /*
 Целочисленные типы данных:
 ------------------------------------------------------------------------------
-integer (int) - 4 bytes, signed
-biginteger (bigint) - 8 bytes, signed
-serial - 4 bytes, unsigned, autoincrement
-bigserial - 8 bytes, unsigned, autoincrement
-smallint - 2 bytes, signed
-smallserial - 2 bytes, unsigned, autoincrement
+integer (int) - 4 bytes, signed                | 123, -5
+biginteger (bigint) - 8 bytes, signed          | 123, -5
+serial - 4 bytes, unsigned, autoincrement      | -
+bigserial - 8 bytes, unsigned, autoincrement   | -
+smallint - 2 bytes, signed                     | 123, -5
+smallserial - 2 bytes, unsigned, autoincrement | 123, -5
 
-numeric (decimal) - accurate floating point
-real - 4 bytes floating point
-double precision - 8 bytes floating point
+numeric (decimal) - accurate floating point    | 2.5, -0.89
+real - 4 bytes floating point                  | 2.5, -0.89
+double precision - 8 bytes floating point      | 2.5, -0.89
 
 Текстовые типы данных:
 ------------------------------------------------------------------------------
-text - variable-length string
-varchar(n) - fixed-length string of n characters
-char(n) - fixed-length string of n characters (add spaces)
+text - variable-length string                              | 'Hello, world!' 
+varchar(n) - fixed-length string of n characters           | 'Hello, world!' 
+char(n) - fixed-length string of n characters (add spaces) | 'Hello, world!' 
 
 Типы для дат и времени:
 ------------------------------------------------------------------------------
-date - date
-timestamp - date and time
-interval - time span
-timestamptz - date and time with time zone
+date - date                                 | '2025-10-06' '20251006'
+timestamp - date and time                   | '2025-10-06 12:00:00'
+interval - time span                        | '1 day 2 hours' '30 minutes'
+timestamptz - date and time with time zone  | '2025-10-06 12:00:00+03'
 
 Дополнтельные типы данных:
 ------------------------------------------------------------------------------
